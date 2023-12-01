7df369144f62450fde7383f93e1ba074845fe758
Naveed Waddo <109472792+NaveedWaddo@users.noreply.github.com>
2023-12-01 14:33:21 +0530

    Create 10.Minimum Window Substring.cpp

---
+++ 10.Minimum Window Substring.cpp
@@ -0,0 +1,56 @@
+//Link : https://leetcode.com/problems/minimum-window-substring/description/
+
+class Solution{
+public:
+    string minWindow(string s, string t){
+        int m = s.size();
+        int n = t.size();
+
+        unordered_map<char,int>mp;
+        for(int i=0; i<n; i++){
+            mp[t[i]]++;
+        }
+
+        int i=0;
+        int j=0;
+        int count = mp.size();
+        int mini = INT_MAX;
+        string res = "";
+        int start = 0;
+
+        while(j < m){
+            if(mp.find(s[j]) != mp.end()){
+                mp[s[j]]--;
+                if(mp[s[j]] == 0){
+                    count--;
+                }
+            }
+
+            if(count == 0){
+                if(mini > j-i+1){
+                    mini=j-i+1;
+                    res = s.substr(i,mini);
+                }
+
+                while(i<=j && count==0){
+                    if(mp.find(s[i]) != mp.end()){
+                        mp[s[i]]++;
+                        if(mp[s[i]] == 1){
+                            count++;
+                            if(j-i+1 < mini){
+                                mini = j-i+1;
+                                start = i;
+                            }
+                        }
+                    }
+                    i++;
+                }
+            }
+            j++;
+        }
+
+        if(mini == INT_MAX) return "";
+
+        return s.substr(start,mini);
+    }
+};
